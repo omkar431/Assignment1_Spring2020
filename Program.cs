6f73a34ed75c8d1803c8ba4227d624a3aa06216d
Omkar Modak <omodak@mail.usf.edu>
2020-02-09 17:08:51 -0500

    Added code template

--- Program.cs
+++ Program.cs
@@ -8,9 +8,26 @@ namespace Assignment1_Spring2020_omkar
         {
             int n = 5;
             PrintPattern(n);
+            Console.WriteLine("\n");
 
             int n2 = 6;
             PrintSeries(n2);
+            Console.WriteLine("\n");
+
+            string s = "09:15:35PM";
+            string t = UsfTime(s);
+            Console.WriteLine(t);
+            Console.WriteLine("\n");
+
+
+            int n3 = 110;
+            int k = 11;
+            UsfNumbers(n3, k);
+            Console.WriteLine("\n");
+
+
+            string[] words = new string[] { "abcd", "dcba", "lls", "s", "sssll" };
+            PalindromePairs(words);
 
 
         }
@@ -20,27 +37,19 @@ namespace Assignment1_Spring2020_omkar
 
             try
             {
-                int val;
-                int i; // Declaring integers
-                val = n; // Assigning the value of n to val and we will use val in for loop
-                for (i = 0; i < n; i++)
-                {
-                    Console.Write(val);  
-                    val = val - 1;
+                int val = n; // Assigning the value of n to val and we will use val in for loop
 
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(val);
+                    val--;
                 } // printing n to 1 by decreasing the value of n by 1 each time before printing on same line
-                n = n - 1; // reducing value of n by 1 so in next for loop pattern will start from n-1 and so on.
-                if (n == 0)
+                Console.WriteLine("");
+                n--; // reducing value of n by 1 so in next for loop pattern will start from n-1 and so on.
+                if (n != 0)
                 {
-                    Console.WriteLine(""); // if n reches to 0 then it will print blank
+                    PrintPattern(n);
                 }
-                else
-                {
-                    Console.WriteLine("\n");
-                    PrintPattern(n); // recursion is used calling same function so function will start again.
-
-                }
-                //Write your code here .!
             }
             catch
             {
@@ -52,19 +61,72 @@ namespace Assignment1_Spring2020_omkar
 
         private static void PrintSeries(int n2)
         {
-            int x;
-            int sum;
-            int i; // Declaring variables
-            sum = 0;
-            x = 1; // Assigning 0 to sum and 1 to x so 
+            int x,
+                sum = 0;
 
-            for (i = 0; i <= n2; i++) // using this we will update sum value as per n2. 
+            x = 1; // Assigning 0 to sum and 1 to x so 
+            for (int i = 0; i < n2; i++) // using this we will update sum value as per n2. 
             {
                 sum = sum + x;
-                x = x + 1;
-                Console.Write(sum);
+                x++;
+                Console.Write(sum + (i < n2-1 ? "," : ""));
+            }
+            
+        }
+
+        public static string UsfTime(string s)
+        {
+            try
+            {
+                //Write your code here .!!
+            }
+            catch
+            {
+                Console.WriteLine("Exception Occured while computing UsfTime");
+            }
+            return null;
+        }
+
+
+        public static void UsfNumbers(int n3, int k)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing UsfNumbers()");
             }
         }
+
+
+
+        public static void PalindromePairs(string[] words)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+
+                Console.WriteLine("Exception occured while computing     PalindromePairs()");
+            }
+        }
+
+        public static void Stones(int n4)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing Stones()");
+            }
+        }
+
 
     }
 }
